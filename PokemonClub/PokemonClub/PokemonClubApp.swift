1a8327371cc96084765b3cec061eaa5d4c823fcf
Hung Nguyen <nynohu@gmail.com>
2023-03-18 20:49:57 +0700

    Initial project

---
+++ PokemonClub/PokemonClub/PokemonClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonClubApp.swift
+//  PokemonClub
+//
+//  Created by NgocHung Nguyen on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
